9ab00688f0f8eaa04918976baac75507e26a854d
Artem <aprricod@gmail.com>
2021-05-10 20:03:05 +0300

    components added

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Info from "./components/info";
+import Form from "./components/form";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Info />
+      </div>
+    );
+  }
+}
+
+export default App;
