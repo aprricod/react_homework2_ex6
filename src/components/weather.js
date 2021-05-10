246b861c23e2dc62a90867066320d7e092dfce21
Artem <aprricod@gmail.com>
2021-05-10 22:38:14 +0300

    рефакторинг кода

--- src/components/weather.js
+++ src/components/weather.js
@@ -1,21 +1,25 @@
 import React from "react";
 
-class Weather extends React.Component {
-  render() {
-    return (
-      <div>
-        {this.props.city && (
-          <div>
-            <p>
-              Местоположение: {this.props.city}, {this.props.country}
-            </p>
-            <p>Температура: {this.props.temp}</p>
-            <p>Давление: {this.props.pressure}</p>
-            <p>Закат солнца: {this.props.sunset}</p>
-          </div>
-        )}
-      </div>
-    );
-  }
+// можно писать через стрелочную функцию и тогда передавать html код внутрь () и не нужно return
+
+// const Weather = props => ()
+
+function Weather(props) {
+  return (
+    <div>
+      {props.city && (
+        <div>
+          <p>
+            Местоположение: {props.city}, {props.country}
+          </p>
+          <p>Температура: {props.temp}</p>
+          <p>Давление: {props.pressure}</p>
+          <p>Закат солнца: {props.sunset}</p>
+        </div>
+      )}
+      <p>{props.error}</p>
+    </div>
+  );
 }
+
 export default Weather;
